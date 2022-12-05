8eb27db1968fdd8ec93525c44c840a28c88e4185
Ashutosh Mishra <ashutoshmishrabanty123@gmail.com>
2022-12-05 14:54:11 +0530

    changes

--- members/models.py
+++ members/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Members(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  middlename= models.CharField(max_length=255)
 # Create your models here.
 
